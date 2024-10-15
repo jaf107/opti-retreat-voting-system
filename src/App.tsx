c13ea60579497a1853b5dd56a8f69d2ec9517f45
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-10-15 23:41:27 +0600

    add voting conclusion page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import useSession from "./hooks/useSession";
 import Homepage from "./components/Homepage";
 import VotingFlow from "./components/VotingFlow";
 import AdminDashboard from "./components/admin/AdminDashboard";
+import VotingConclusion from "./components/VotingConclusion";
 
 export default function App() {
   const session = useSession();
@@ -52,6 +53,7 @@ export default function App() {
               <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/vote/:categoryId" element={<VotingFlow />} />
+                <Route path="/vote/conclusion" element={<VotingConclusion />} />
                 <Route path="/admin/*" element={<AdminDashboard />} />
               </Routes>
             </Box>

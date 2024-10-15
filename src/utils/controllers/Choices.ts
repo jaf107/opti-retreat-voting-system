2b6f4feb7e6982c2eb7eb4ee0374750e4cb05c03
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-10-15 22:53:47 +0600

    hide options from voting option

--- src/utils/controllers/Choices.ts
+++ src/utils/controllers/Choices.ts
@@ -1,11 +1,20 @@
 import { CHOICES } from "../../constants/db.names";
 import { supabase } from "../supabaseClient";
 
-export const fetchChoices = async (categoryId: string) => {
+export const fetchAllChoices = async (categoryId: string) => {
   const { data, error } = await supabase
     .from(CHOICES)
     .select("*")
     .eq("category_id", categoryId);
+
+  return { data, error };
+};
+export const fetchVotingChoices = async (categoryId: string) => {
+  const { data, error } = await supabase
+    .from(CHOICES)
+    .select("*")
+    .eq("category_id", categoryId)
+    .eq("hidden", false);
 
   return { data, error };
 };

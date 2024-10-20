ba9cf94473e4f5aefb4990f790dd1dbf4505e510
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-10-20 11:23:17 +0600

    add bg to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -13,8 +14,11 @@ import {
   Heading,
   Text,
 } from "@chakra-ui/react";
-import { AnimatePresence } from "framer-motion";
 import { SessionProvider } from "./contexts/SessionContext";
+import {
+  BackgroundProvider,
+  useBackground,
+} from "./contexts/BackgroundContext";
 import useSession from "./hooks/useSession";
 
 import Homepage from "./components/Homepage";
@@ -22,21 +26,89 @@ import VotingFlow from "./components/VotingFlow";
 import AdminDashboard from "./components/admin/AdminDashboard";
 import VotingConclusion from "./components/VotingConclusion";
 import AnnouncementConclusion from "./components/admin/announcement/AnnouncementConclusion";
-import { BackgroundProvider } from "./contexts/BackgroundContext";
 
-const AnimatedRoutes = () => {
+const AppContent = () => {
+  const { currentBackground, setBackgroundType } = useBackground();
   const location = useLocation();
 
+  useEffect(() => {
+    if (location.pathname.startsWith("/vote")) {
+      setBackgroundType("voting");
+    } else if (
+      location.pathname.startsWith("/admin/announce") ||
+      location.pathname === "/adios"
+    ) {
+      setBackgroundType("announcement");
+    } else {
+      setBackgroundType("default");
+    }
+  }, [location.pathname, setBackgroundType]);
+
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/vote/:categoryId" element={<VotingFlow />} />
-        <Route path="/vote/conclusion" element={<VotingConclusion />} />
-        <Route path="/adios" element={<AnnouncementConclusion />} />
-        <Route path="/admin/*" element={<AdminDashboard />} />
-      </Routes>
-    </AnimatePresence>
+    <Flex flexDirection="column" minHeight="100vh">
+      <Box
+        position="fixed"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        backgroundImage={`url("${currentBackground}")`}
+        backgroundSize="contain"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        zIndex={-1}
+      />
+      <Flex
+        as="nav"
+        p={4}
+        alignItems="center"
+        bg="white"
+        boxShadow="sm"
+        zIndex={1}
+      >
+        <Flex flex="1" alignItems="center" justifyContent="center">
+          <Link to="/">
+            <Image
+              src="/optimizely-logo.png"
+              height="24px"
+              alt="Optimizely Logo"
+              mr={4}
+            />
+          </Link>
+          <Heading as="h2" size="md" fontWeight="bold">
+            UnOptimized Awards 2024
+          </Heading>
+        </Flex>
+        <Flex justifyContent="flex-end">
+          <Link
+            to="/admin"
+            style={{
+              cursor: "default",
+            }}
+          >
+            <Text color="white">A</Text>
+          </Link>
+        </Flex>
+      </Flex>
+      <Box flex="1" overflowY="auto" p={4} position="relative" zIndex={1}>
+        <Box
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg="rgba(255, 255, 255, 0.9)"
+          zIndex={-1}
+        />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/vote/:categoryId" element={<VotingFlow />} />
+          <Route path="/vote/conclusion" element={<VotingConclusion />} />
+          <Route path="/adios" element={<AnnouncementConclusion />} />
+          <Route path="/admin/*" element={<AdminDashboard />} />
+        </Routes>
+      </Box>
+    </Flex>
   );
 };
 
@@ -48,42 +120,7 @@ export default function App() {
       <SessionProvider value={session}>
         <BackgroundProvider>
           <Router>
-            <Flex flexDirection="column" minHeight="100vh">
-              <Flex
-                as="nav"
-                p={4}
-                alignItems="center"
-                bg="white"
-                boxShadow="sm"
-              >
-                <Flex flex="1" alignItems="center" justifyContent={"center"}>
-                  <Link to="/">
-                    <Image
-                      src="/optimizely-logo.png"
-                      height="24px"
-                      alt="Optimizely Logo"
-                      mr={4}
-                    />
-                  </Link>
-                  <Heading as="h2" size="md" fontWeight="bold">
-                    UnOptimized Awards 2024
-                  </Heading>
-                </Flex>
-                <Flex justifyContent={"flex-end"}>
-                  <Link
-                    to="/admin"
-                    style={{
-                      cursor: "default",
-                    }}
-                  >
-                    <Text color={"white"}>A</Text>
-                  </Link>
-                </Flex>
-              </Flex>
-              <Box flex="1" overflowY="auto" p={4} bg="gray.100">
-                <AnimatedRoutes />
-              </Box>
-            </Flex>
+            <AppContent />
           </Router>
         </BackgroundProvider>
       </SessionProvider>

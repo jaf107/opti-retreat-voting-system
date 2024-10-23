78f96701fe6b571cc6869299c302148f457d2ef0
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-10-23 15:25:10 +0600

    fix height issue

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const AppContent = () => {
   }, [location.pathname, setBackgroundType]);
 
   return (
-    <Flex height={"100vh"} flexDirection="column">
+    <Flex minHeight={"100vh"} flexDirection="column">
       <Box
         position="fixed"
         top={0}
